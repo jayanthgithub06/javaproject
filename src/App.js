856ef920262442d10d9f6b58981bb956fdcfcbf7
jayanthmysuru <143187659+jayanthmysuru@users.noreply.github.com>
2024-04-04 17:46:10 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          This is a new App...
+          K.jayanth
           Yo!! ❤🤞💕🤷‍♀️
         </p>
       </div>
